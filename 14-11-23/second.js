1ba0af5d1e276bc6c4233123d0098f2e82ffddca
Rana Salman <70133516@student.uol.edu.pk>
2023-11-14 09:22:26 +0500

    javascript class task 2nd

--- 14-11-23/second.js
+++ 14-11-23/second.js
@@ -12,19 +12,19 @@
 
 //Task 02
 
-var age = 10;
-if( age >=65 ){
-    console.log("You get your income from your pension");
-}
-else if( age < 65 && age > 18){
-    console.log("Each month you get a salary");
-}
-else if( age < 18){
-    console.log("you get an allowance");
-}
-else{
-    console.log("The value of age variable is not nummerical");
-}
+// var age = 10;
+// if( age >=65 ){
+//     console.log("You get your income from your pension");
+// }
+// else if( age < 65 && age > 18){
+//     console.log("Each month you get a salary");
+// }
+// else if( age < 18){
+//     console.log("you get an allowance");
+// }
+// else{
+//     console.log("The value of age variable is not nummerical");
+// }
 
 
 
@@ -44,3 +44,33 @@ else{
 //     default:
 //         console.log('No Medal');
 // }
+
+
+
+//Task 04
+var day = 'sunday';
+switch (day){
+    case 'monday':
+        console.log('First day of week');
+        break;
+    case 'tuesday':
+        console.log('Second day of week');
+        break;
+    case 'wednesday':
+        console.log('Third day of week');
+        break;
+    case 'thursday':
+        console.log('Fourth day of week');
+        break;
+    case 'friday':
+        console.log('Fifth day of week');
+        break;
+    case 'saturday':
+        console.log('Sixth day of week');
+        break;
+    case 'sunday':
+        console.log('Seventh day of week');
+        break;
+    default:
+        console.log('There is no such day in week');
+}
