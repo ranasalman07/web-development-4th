ce5e0a3e13d574823a8147b6c0419e7fa72d3f1c
Rana Salman <70133516@student.uol.edu.pk>
2023-11-14 09:14:17 +0500

    javascript class task

---
+++ 14-11-23/second.js
@@ -0,0 +1,46 @@
+//Task 01
+
+// var result = 50;
+// if (result > 40){
+//     console.log("Congratulations you are passed");
+
+// }else {
+//     console.log("Unfortunately you did not pass");
+// }
+
+
+
+//Task 02
+
+var age = 10;
+if( age >=65 ){
+    console.log("You get your income from your pension");
+}
+else if( age < 65 && age > 18){
+    console.log("Each month you get a salary");
+}
+else if( age < 18){
+    console.log("you get an allowance");
+}
+else{
+    console.log("The value of age variable is not nummerical");
+}
+
+
+
+//Task 03
+
+// var place = 'first';
+// switch(place){
+//     case 'first':
+//         console.log('Gold')
+//         break;
+//     case 'second':
+//         console.log('Silver')
+//         break;
+//     case 'third':
+//         console.log('Bronze')
+//         break;
+//     default:
+//         console.log('No Medal');
+// }
