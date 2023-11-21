487f7e42dbde52811829ff86302a9dbb24613a8d
Rana Salman <70133516@student.uol.edu.pk>
2023-11-21 12:02:56 +0500

    Class Work

---
+++ 21-11-23/class.js
@@ -0,0 +1,71 @@
+//Task 01
+// Math.random();
+// Math.trunc(decimal);
+// var decimal = Math.trunc(Math.random()*1000);
+// console.log(decimal);
+
+
+
+//Task 02
+// var string="ho-ho-ho";
+// console.log(string.indexOf("h"));
+// console.log(string.indexOf("o"));
+// console.log(string.indexOf("-"));
+// console.log(string.lastIndexOf('h'));
+// console.log(string.lastIndexOf('o'));
+// console.log(string.lastIndexOf('-'));
+
+
+
+//Task 03
+// var string="ho-ho-ho";
+// console.log(string.split("-"));
+
+
+
+//Task 04
+// var greet="Hello, ";
+// console.log(greet.toUpperCase());
+// console.log(greet.toLowerCase()); 
+
+
+
+//Task 05
+// var clothes=[];
+// clothes.push("Coat");
+// clothes.push("Pent");
+// clothes.push("Shirt");
+// clothes.push("Tie");
+// clothes.push("Waistcoat");
+// clothes.pop("waistcoat");
+// clothes.push("overcoat");
+// console.log(clothes);
+// console.log(clothes[2]);
+
+
+
+//Task 06
+// var favcar ={};
+// favcar.color="blue";
+// favcar.convertile=true;
+// console.log(favcar);
+
+
+
+//Task 07
+var car={};
+car.mileage=19750;
+car.color="Black";
+console.log(car);
+car.turnTheKey=function( i=1){
+    if(i === 1 ){
+    console.log("The engine is running");
+}
+else
+}
+car.lightsOn=function(){
+    console.log("THe lights are on");
+}
+console.log(car);
+car.turnTheKey(1);
+car.lightsOn();
