6210307e73de67d96dbcd8f176f7e1a9b127ea77
Rana Salman <70133516@student.uol.edu.pk>
2023-12-05 09:42:23 +0500

    Class Work 2nd 05-12-23

---
+++ 05-12-23/class.js
@@ -0,0 +1,29 @@
+//Task 01
+// let greet = "Hello";
+// let place = "World";
+// console.log(`${greet}${ place}!`);
+// console.log(`${1+1+1+1}stars!`);
+
+
+
+//Task 02 Spread Operator
+// let top3 = [
+//     " Salman",
+//     " Tayyab",
+//     " Bilal"
+// ];
+// function showItinerary(place1, place2, place3){
+//     console.log("Visit" + place1);
+//     console.log("Then Visit" + place2);
+//     console.log("Finish with a visit to" + place3);
+// }
+// showItinerary(...top3);
+
+
+
+//Task 03 Rest Operator
+// function addTaxToPrices(taxRate,...itemsBought){
+//     return itemsBought.map(item => taxRate * item)
+// }
+// let shoppingCart = addTaxToPrices(1.1,46,89,35,79);
+// console.log(shoppingCart);
